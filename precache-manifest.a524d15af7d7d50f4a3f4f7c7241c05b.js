72bce9be9a1017106a056ccf8ade0c7fe7281176
Zack Moscow <zmoscow@gmail.com>
2020-02-09 17:03:33 -0600

    Updates

---
+++ precache-manifest.a524d15af7d7d50f4a3f4f7c7241c05b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbebb1210eae3ca1388ac0387d5e405",
+    "url": "zackmoscow.github.io/index.html"
+  },
+  {
+    "revision": "a4d15ee22232cc6fdf2b",
+    "url": "zackmoscow.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1105b4fe72bd74085c99",
+    "url": "zackmoscow.github.io/static/js/2.c2bbbca7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "zackmoscow.github.io/static/js/2.c2bbbca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4d15ee22232cc6fdf2b",
+    "url": "zackmoscow.github.io/static/js/main.3412ef97.chunk.js"
+  },
+  {
+    "revision": "eb9ddba5c346191ce14b",
+    "url": "zackmoscow.github.io/static/js/runtime-main.65914ccd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "zackmoscow.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
